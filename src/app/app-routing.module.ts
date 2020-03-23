318ecfb27a2211fe0daa1bb19088fb462a2f77b1
deji adesoga <done2stay@gmail.com>
2020-03-23 10:38:25 +0100

    Made Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandComponent } from './parent/land/land.component';
 import { SteptwoComponent } from './steptwo/steptwo.component';
-import { StepthreeComponent } from './stepthree/stepthree.component';
 
 
 const routes: Routes = [
   {path: 'twolanding', component: SteptwoComponent},
-  {path: 'threelanding', component: StepthreeComponent},
-  {path: 'landing', component: LandComponent},
-  { path: '', redirectTo: '/landing', pathMatch: 'full' }
+  // { path: '', redirectTo: '/twolanding', pathMatch: 'full' }
 ];
 
 @NgModule({
